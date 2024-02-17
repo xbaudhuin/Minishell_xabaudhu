150fd0128d7f255c77126be7db356423aad23507
Leo Doyen--Clement <ldoyen--@made-f0Br7s14.clusters.42paris.fr>
2024-02-17 16:02:26 +0100

    adding get_expanded-env which copy old env and have one more slot if old env is full #4

--- include/env.h
+++ include/env.h
@@ -10,6 +10,8 @@ typedef struct s_env
 }			t_env;
 
 t_env	create_env(const char **main_env);
-
+void	free_env(t_env env);
+char	*ft_getenv(const char *name, const t_env env);
+t_env	get_expended_env(t_env old_env);
 
 #endif
---
+++ src/mandatory/env/get_expanded_env.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_empty_address.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldoyen-- <ldoyen--@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/16 20:08:34 by ldoyen--          #+#    #+#             */
+/*   Updated: 2024/02/16 20:08:36 by ldoyen--         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "env.h"
+
+static int	is_env_full(const t_env env)
+{
+	size_t	var_num;
+
+	var_num = 0;
+	while (var_num < env.allocated_size - 1)
+	{
+		if (env.variables[var_num] == NULL)
+		{
+			return (FALSE);
+		}
+		++var_num;
+	}
+	return (TRUE);
+}
+
+static size_t	extanded_size(const t_env old_env)
+{
+	return (old_env.allocated_size + 1);
+}
+
+static char	**set_new_variable_table(const size_t new_allocated_size)
+{
+	char	**new_table;
+
+	new_table = ft_calloc(new_allocated_size, sizeof (*new_table));
+	return (new_table);
+}
+
+static char	**copy_old_env(t_env old_env,
+		const size_t new_allocated_size)
+{
+	char	**new_table;
+	size_t	var_num;
+
+	new_table = set_new_variable_table(new_allocated_size);
+	if (new_table == NULL)
+	{
+		return (NULL);
+	}
+	var_num = 0;
+	while (var_num < old_env.allocated_size)
+	{
+		new_table[var_num] = old_env.variables[var_num];
+		++var_num;
+	}
+	new_table[var_num] = NULL;
+	free(old_env.variables);
+	return (new_table);
+}
+
+t_env	get_expended_env(t_env old_env)
+{
+	t_env	new_env;
+	size_t	new_allocated_size;
+
+	if (is_env_full(old_env) == FALSE)
+	{
+		return (old_env);
+	}
+	new_allocated_size = extanded_size(old_env);
+	new_env.allocated_size = new_allocated_size;
+	new_env.variables = copy_old_env(old_env,
+			(const size_t) new_allocated_size);
+	return (new_env);
+}
+
+// int main(int ac, char **av, char **env)
+// {
+// 	t_env	my_env;
+
+// 	(void)ac;
+// 	(void)av;
+// 	my_env = create_env((const char **)env);
+// 	size_t num = 0;
+// 	printf("env alloc size = %zu\n\n", my_env.allocated_size);
+// 	printf("is env full = %d\n\n", is_env_full(my_env));
+// 	while (num < my_env.allocated_size)
+// 	{
+// 		printf("%s\n", my_env.variables[num]);
+// 		++num;
+// 	}
+// 	my_env = get_expended_env(my_env);
+// 	num = 0;
+// 	printf("env alloc size = %zu\n\n", my_env.allocated_size);
+// 	printf("is env full = %d\n\n", is_env_full(my_env));
+// 	while (num < my_env.allocated_size)
+// 	{
+// 		printf("%s\n", my_env.variables[num]);
+// 		++num;
+// 	}
+// 	free_env(my_env);
+// 	return (0);
+// }
